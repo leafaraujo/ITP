811b5b8cab1f37d5dd09781a550d9cec0ce2eb4f
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2023-08-19 16:31:16 -0300

    Update mercado_itp.c

--- Introduction/mercado_itp.c
+++ Introduction/mercado_itp.c
@@ -8,7 +8,7 @@ int main(){
     meses = (dias / 29 > 0 )? dias / 30 - (anos * 12) : 0;
     dias_restantes = (dias > 29)? dias % (dias/30) - (anos * 5) : dias; 
     meses_restantes = (dias_restantes < 0)?  dias_restantes / 30 * (-1): meses;
-    dias_restantes = (dias_restantes < 0)? (dias_restantes ) + ((meses_restantes*30) +30) : dias_restantes;
+    dias_restantes = (dias_restantes < 0)? (dias_restantes ) + ((meses - meses_restantes)*30) : dias_restantes;
     printf("%d ano(s)\n", anos);
     printf("%d mes(es)\n", meses_restantes);
     printf("%d dia(s)\n", dias_restantes);
