838a64134971ef7e57741c7126fef4f2ce7478fc
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2023-08-21 18:17:53 -0300

    Add files via upload

---
+++ 2.Introduction/mercado_itp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int dias, meses, anos, dias_restantes, meses_restantes;
+    scanf("%d", &dias);
+    anos = (dias / 360 > 0)? dias / 360 : 0;
+    meses = (dias / 29 > 0 )? dias / 30 - (anos * 12) : 0;
+    dias_restantes = (dias > 29)? dias % (dias/30) - (anos * 5) : dias; 
+    meses_restantes = (dias_restantes < 0)?  dias_restantes / 30 * (-1): meses;
+    dias_restantes = (dias_restantes < 0)? (dias_restantes ) + ((meses - meses_restantes)*30) : dias_restantes;
+    printf("%d ano(s)\n", anos);
+    printf("%d mes(es)\n", meses_restantes);
+    printf("%d dia(s)\n", dias_restantes);
+    
+    return 0;
+}
---
+++ 2.Introduction/multiplos_3_not_5.md
@@ -0,0 +1,3 @@
+# Quero múltiplos de 3, mas não de 5 
+
+Escreva um programa em C que leia um número inteiro n (assuma n maior ou igual a 0). Em seguida, o programa deve escrever "Resposta: ", seguido de 1 (verdadeiro) caso n for múltiplo de 3, não for múltiplo de 5 e for diferente de 0; ou seguido de 0(falso) caso contrário.
