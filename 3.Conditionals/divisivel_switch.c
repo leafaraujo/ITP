5dd0ea0ba52c22d7c688c183c759765d2d157773
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2023-08-22 12:18:37 -0300

    Update divisivel_switch.c

--- 3.Conditionals/divisivel_switch.c
+++ 3.Conditionals/divisivel_switch.c
@@ -5,7 +5,7 @@ int main() {
     int a,b,c;
     scanf("%d %d", &a, &b);
     switch (a % b){
-        case 0: printf("Resultado: %d", a/b);
+        case 0: printf("Resultado: %d", a/b); break;
     }
 
     return 0;
